3d49219a6f3abf60c8d20800520d6e61eb42c32f
Fardin Haque <fardinhaque60@gmail.com>
2025-03-23 14:07:57 -0700

    clean/ update homepage and navbar

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from "../shared/footer/footer.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent, MatIconModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
