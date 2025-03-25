e238855a4941571801b5447d383ae67a434d3a45
Fardin Haque <fardinhaque60@gmail.com>
2025-03-24 19:16:51 -0700

    service for making post and get requests to any api endpoint

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {}
+
+  getRegRequest(endpoint: string): Observable<any> {
+    return this.http.get<any>(endpoint);
+  }
+
+  postRegRequest(endpoint: string, payload: any): Observable<any> {
+    return this.http.post<any>(endpoint, payload);
+  }
+}
