4abc9359d456eadd4f0c3a49dd1daeef8dafad36
Fardin Haque <fardinhaque60@gmail.com>
2025-03-26 23:58:28 -0700

    client requests to read and write prayer time caching

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from "../shared/footer/footer.component";
 import { MatIconModule } from '@angular/material/icon';
 import { ApiService } from '../service/api.service';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
   imports: [NavbarComponent, FooterComponent, MatIconModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
+  changeDetection: ChangeDetectionStrategy.Default,
+  standalone: true,
 })
 export class HomepageComponent {
-  constructor(private apiService: ApiService) {};
-  tempVar = "";
+  constructor(private cdRef: ChangeDetectorRef, private apiService: ApiService) {};
+  todayDate: string = ''; // todays date in DD-MM-YYYY format
 
   adhanTimes = {
     fajr: 'X:XX',
@@ -20,7 +24,7 @@ export class HomepageComponent {
     shafiAsr: 'X:XX',
     hanafiAsr: 'X:XX',
     maghrib: 'X:XX',
-    isha: 'X:XX'
+    isha: 'X:XX',
   };
   iqamahTimes = {
     fajr: '6:35 AM',
@@ -28,13 +32,13 @@ export class HomepageComponent {
     dhuhr2: '3:00 PM',
     shafiAsr: '5:00 PM',
     hanafiAsr: '5:45 PM',
-    maghrib: 'Loading...',
+    maghrib: 'XX:XX',
     isha: '8:45 PM'
   };
 
+  // returns data object DD-MM-YYYY format for api
   formatDate(date: Date): string {
     const stringDate = date.toISOString().split('T')[0];
-    // format the date into DD-MM-YYYY
     const dateParts = stringDate.split('-');
     const day = dateParts[2];
     const month = dateParts[1];
@@ -44,7 +48,8 @@ export class HomepageComponent {
     return formattedDate;
   };
 
-  convertTo12HourFormat(time: string): string {
+  // returns XX:XX AM/PM format in 12 hour time given 24 hour time string to render in frontend
+  convertTo12HourTime(time: string): string {
     const [hour, minute] = time.split(':');
     let hourInt = parseInt(hour);
     const period = hourInt >= 12 ? 'PM' : 'AM';
@@ -53,6 +58,7 @@ export class HomepageComponent {
     return formattedTime;
   };
 
+  // returns date object of current date in PST
   getCurrentPSTDate(): Date {
     const currentDate = new Date();
     const utcOffset = -7; // UTC-7 for Pacific Standard Time (PST)
@@ -60,73 +66,92 @@ export class HomepageComponent {
     return pstDate;
   };
 
-  fetchPrayerTimesFromApi() {
-    console.log('fetching prayer times from api');
-    const currentDate = this.getCurrentPSTDate();
-    const formattedDate = this.formatDate(currentDate); // TODO add this to api url
+  // fetches prayer times from api and updates adhanTimes object
+  async fetchPrayerTimesFromApi() {
+    console.log('CLIENT: fetching prayer times from api');
 
     // get adhan times
-    const prayerTimeApiURL = `https://api.aladhan.com/v1/timingsByCity/${formattedDate}?city=San%Jose&country=USA&method=2&shafaq=general&calendarMethod=UAQ`;
-    this.apiService.getRegRequest(prayerTimeApiURL)
-      .subscribe({
-        next: (response) => {
-          const prayerTimings = response.data.timings;
-          /* 
-            very strange bug in javascript where looping through the prayerTimings object 
-            causes times to convert with AM append to it in browser console, but find in terminal browser.
-            Assuming this is something wrong with how terminal and browser are running code, but going to 
-            avoid this loop and just convert it individually below.
-          */
-          /* 
-          for (const [key, value] of Object.entries(prayerTimings)) {
-            const convertedTime = this.convertTo12HourFormat(value as string);
-            prayerTimings[key] = convertedTime; 
-          } */
-
-          // set prayer times
-          this.adhanTimes.fajr = this.convertTo12HourFormat(prayerTimings.Fajr);
-          this.adhanTimes.dhuhr = this.convertTo12HourFormat(prayerTimings.Dhuhr);
-          this.adhanTimes.shafiAsr = this.convertTo12HourFormat(prayerTimings.Asr);
-          this.adhanTimes.maghrib = this.convertTo12HourFormat(prayerTimings.Maghrib);
-          this.iqamahTimes.maghrib = this.convertTo12HourFormat(prayerTimings.Maghrib);
-          this.adhanTimes.isha = this.convertTo12HourFormat(prayerTimings.Isha);
-        },
-        error: (error) => {
-          console.error(error);
-        }
+    const prayerTimeApiUrl = `https://api.aladhan.com/v1/timingsByCity/${this.todayDate}?city=San%Jose&country=USA&method=2&shafaq=general&calendarMethod=UAQ`;
+    await firstValueFrom(this.apiService.getRequest(prayerTimeApiUrl))
+      .then((response) => {
+        const prayerTimings = response.data.timings;
+        this.adhanTimes.fajr = this.convertTo12HourTime(prayerTimings.Fajr);
+        this.adhanTimes.dhuhr = this.convertTo12HourTime(prayerTimings.Dhuhr);
+        this.adhanTimes.shafiAsr = this.convertTo12HourTime(prayerTimings.Asr);
+        this.adhanTimes.maghrib = this.convertTo12HourTime(prayerTimings.Maghrib);
+        this.iqamahTimes.maghrib = this.convertTo12HourTime(prayerTimings.Maghrib);
+        this.adhanTimes.isha = this.convertTo12HourTime(prayerTimings.Isha);
+      }).catch((error) => {
+        console.error("CLIENT: error: ", error);
       });
 
     // get hanafi asr time
-    const hanafiAsrTimeApiURL = `https://api.aladhan.com/v1/timingsByCity/${formattedDate}?city=San%Jose&country=USA&method=2&shafaq=general&calendarMethod=UAQ&school=1`;
-    this.apiService.getRegRequest(hanafiAsrTimeApiURL)
-      .subscribe({
-        next: (response) => {
-          const prayerTimings = response.data.timings;
-
-          this.adhanTimes.hanafiAsr = this.convertTo12HourFormat(prayerTimings.Asr);
-        },
-        error: (error) => {
-          console.error(error);
-        }
+    const hanafiAsrTimeApiUrl = `https://api.aladhan.com/v1/timingsByCity/${this.todayDate}?city=San%Jose&country=USA&method=2&shafaq=general&calendarMethod=UAQ&school=1`;
+    await firstValueFrom(this.apiService.getRequest(hanafiAsrTimeApiUrl))
+      .then((response) => {
+        const prayerTimings = response.data.timings;
+        this.adhanTimes.hanafiAsr = this.convertTo12HourTime(prayerTimings.Asr);
+      }).catch((error) => {
+        console.error("CLIENT: error: ", error);
       });
   }
 
+  // updates prayer times given db cache response
+  async updatePrayerTimes(prayerTimesData: any) {
+    const postPrayerTimesApiUrl = "api/postPrayerTimes";
+
+    // if response is empty that means no db entry for today
+    if (!prayerTimesData) {
+      console.log("CLIENT: no prayer times for today cached, fetching from api");
+      await this.fetchPrayerTimesFromApi();
+      // add date field to prayerTimes
+      const prayerTimes = {
+        ...this.adhanTimes,
+        date: this.todayDate
+      }; 
+      // post these prayer times to db
+      this.apiService.postRequest(postPrayerTimesApiUrl, { prayerTimes })
+        .subscribe({
+          next: (response) => {
+            console.log("CLIENT: prayer times updated in db: ", JSON.stringify(response));
+          },
+          error: (error) => {
+            console.error(error);
+          }
+        });
+    } else { // if prayer times data is in db, read it from db
+      console.log("CLIENT: prayer times for today cached in db, loading times from db: \n" + JSON.stringify(prayerTimesData));
+      this.adhanTimes.fajr = prayerTimesData.fajr;
+      this.adhanTimes.dhuhr = prayerTimesData.dhuhr;
+      this.adhanTimes.shafiAsr = prayerTimesData.shafiAsr;
+      this.adhanTimes.hanafiAsr = prayerTimesData.hanafiAsr;
+      this.adhanTimes.maghrib = prayerTimesData.maghrib;
+      this.iqamahTimes.maghrib = prayerTimesData.maghrib;
+      this.adhanTimes.isha = prayerTimesData.isha;
+    }
+  }
+
   ngOnInit() {
-    // TODO add supabase methods to push prayer time to db if not current date
-    // if it has current date just pull it from db
-    // see homepage component.html for todo on adding tooltip
-    this.fetchPrayerTimesFromApi();
-    // write to db
-    console.log("test call to db");
-    this.apiService.getPrayerTimes()
-      .subscribe({
-        next: (response) => {
-          this.tempVar = JSON.stringify(response);
-        }
-        , 
-        error: (error) => {
-          console.error(error);
-        }
-      });
+    /* TODO
+     see homepage component.html for todo on adding tooltip
+     delete entries for adhan times if old (add something to !prayerTimesData case for deleting everything in db before post request)
+     add iqamah times to db
+    */
+    const currentDate = this.getCurrentPSTDate();
+    this.todayDate = this.formatDate(currentDate); // initialize todayDate for all function calls to use
+    const getPrayerTimesTodayApiUrl = "api/getPrayerTimes?todayDate=" + this.todayDate;
+
+    console.log("CLIENT: checking db if it has prayer time for today (DD-MM-YYYY): ", this.todayDate);
+    if (typeof window !== 'undefined') {
+      this.apiService.getRequest(getPrayerTimesTodayApiUrl)
+        .subscribe({
+          next: (response) => {
+            this.updatePrayerTimes(response[0]);
+          },
+          error: (error) => {
+            console.error(error);
+          }
+        });
+    }
   }
 }
