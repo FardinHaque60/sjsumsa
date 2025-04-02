c3a91683e7c6646fa388a5fb11aa29513012b776
Fardin Haque <fardinhaque60@gmail.com>
2025-04-02 08:24:50 -0400

    interfaces for adhan/ iqamah

---
+++ src/interfaces/prayerTimeInt.ts
@@ -0,0 +1,41 @@
+export interface adhanTimesInt {
+    fajr: string;
+    dhuhr: string;
+    shafiAsr: string;
+    hanafiAsr: string;
+    maghrib: string;
+    isha: string;
+};
+
+export interface iqamahTimesInt {
+    fajr: string,
+    dhuhr: string,
+    dhuhr2?: string, // TODO mark optional?
+    shafiAsr: string,
+    hanafiAsr: string,
+    maghrib: string,
+    isha: string
+};
+
+export interface adhanApiInt {
+    timings: {
+        Fajr: string,
+        Dhuhr: string,
+        Asr: string,
+        Maghrib: string,
+        Isha: string,
+    },
+    date: {
+        gregorian: {
+            date: string,
+        }
+    }
+}
+
+export interface adhanDbInt {
+    fajr: string,
+    dhuhr: string,
+    shafiAsr: string,
+    maghrib: string,
+    isha: string,
+}
