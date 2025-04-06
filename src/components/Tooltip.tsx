26a9ccab80ca539501cacdd5e131213b9ab42968
Fardin Haque <fardinhaque60@gmail.com>
2025-04-06 15:44:24 -0700

    resolved mcs

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,13 +9,8 @@ const Tooltip: React.FC<TooltipProps> = ({ children, text }) => {
   return (
     <div className="relative group">
       {children}
-<<<<<<< HEAD
       <div className="w-50 absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 z-10">
       {text}
-=======
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-4 z-10">
-        {text}
->>>>>>> e1495e0 (added tool tip to prayer times section)
       </div>
     </div>
   );
