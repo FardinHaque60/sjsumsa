08b83749c62957a9b01ae6ed379c05b71ee9f377
Fardin Haque <fardinhaque60@gmail.com>
2025-04-10 18:05:39 -0700

    remove logging

--- src/lib/admin/adminStatus.ts
+++ src/lib/admin/adminStatus.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export async function checkAdminStatus() {
-    const response = await axios.get("api/admin/status", {
-        withCredentials: true
-    });
-    console.log("CLIENT: admin status - ", response.data.authenticated);
-    return response.data.authenticated;
+  const response = await axios.get("api/admin/status", {
+    withCredentials: true
+  });
+  return response.data.authenticated;
 };
