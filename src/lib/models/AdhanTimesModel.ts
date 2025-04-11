f5d89e679e056fee8e6fa4ebe059b273b0789ddb
Fardin Haque <fardinhaque60@gmail.com>
2025-04-11 08:38:49 -0700

    make all adhan time fields required

--- src/lib/models/AdhanTimesModel.ts
+++ src/lib/models/AdhanTimesModel.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const AdhanTimesSchema = new mongoose.Schema({
     fajr: { type: String, required: true },
-    dhuhr: { type: String, required: false },
-    shafiAsr: { type: String, required: false },
+    dhuhr: { type: String, required: true },
+    shafiAsr: { type: String, required: true },
     hanafiAsr: { type: String, required: true }, 
     maghrib: { type: String, required: true },
     isha: { type: String, required: true },
