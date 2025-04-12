2c83dbdf8cfb501e47d60d9cc21dbe8fd515d721
Fardin Haque <fardinhaque60@gmail.com>
2025-04-11 22:23:09 -0700

    sort directly in events endpoint

--- src/app/api/events/read/route.ts
+++ src/app/api/events/read/route.ts
@@ -4,9 +4,9 @@ import EventModel from "@/lib/models/EventModel";
 // GET all events
 export async function GET() {
     try {
-      const events = await EventModel.find({});
-      // sort in descending order by eventDate
-      events.sort((a, b) => new Date(b.eventDate).getTime() - new Date(a.eventDate).getTime());
+      const events = await EventModel.find({})
+        .sort({ eventDate: -1 })
+        .limit(10); // limit to most recent 10 events
 
       return NextResponse.json({body: events}, { status: 200 });
     } catch (error) {
